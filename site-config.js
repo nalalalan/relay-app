26cdd663a81b512bbe10339d621b4f5f9b292435
nalalalan <pham.alann@gmail.com>
2026-04-17 12:47:28 -0400

    fix mobile header, tighten proof section, and repair stripe checkout

--- site-config.js
+++ site-config.js
@@ -1,4 +1,5 @@
 window.AO_CONFIG = {
   STRIPE_URL: "https://buy.stripe.com/bJeaEZb4mf6de64dSi2Nq02",
+  PACKET_CHECKOUT_URL: "https://buy.stripe.com/bJeaEZb4mf6de64dSi2Nq02",
   CLIENT_INTAKE_DESTINATION: ""
 };
